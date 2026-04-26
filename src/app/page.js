2a2247cce2c617a932ad69ae94dfa8b84405e8d9
yousufrahman <ryousuf766@gmail.com>
2026-04-26 21:27:35 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+      <Button >Click me</Button>
+         <Button variant="secondary">Secondary</Button>
+    </div>
+     
+  );
+}
